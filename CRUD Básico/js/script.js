b77b467564c0d16e143d488283b08cf728af70d9
João Victor França <franca.jv@hotmail.com>
2022-12-01 17:49:56 -0300

    Register Function

--- CRUD Básico/js/script.js
+++ CRUD Básico/js/script.js
@@ -5,10 +5,10 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
 const tempClient = {
-    nome: "Gabriel",
-    email: "gabs@hotmail.com",
-    phone: "11998132120",
-    city: "São Paulo"
+    name: 'Gabriel',
+    email: 'gabs@hotmail.com',
+    phone: '11998132120',
+    city: 'São Paulo'
 }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
@@ -36,8 +36,32 @@ const deleteClient = (index) => {
     setLocalStorage(dbClient);
 }
 
+//Interações de usuário
+const fieldsFilled = () => {
+    return document.getElementById('form').reportValidity();
+}
+
+const saveClient = () => {
+    if (fieldsFilled()) {
+        const client = {
+            name: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            phone: document.getElementById('celular').value,
+            city: document.getElementById('cidade').value
+        }
+        createClient(client);
+        clearFields();
+        closeModal();
+    }
+}
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field');
+    fields.forEach(field => field.value = '');
+}
+
 
 // EVENTOS
 document.getElementById('cadastrarCliente').addEventListener('click', openModal);
-
 document.getElementById('modalClose').addEventListener('click', closeModal);
+document.getElementById('salvar').addEventListener('click', saveClient);

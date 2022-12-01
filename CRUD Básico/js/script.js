308efe5abbef1975dd0b95c81dcdba0356a12b22
João Victor França <franca.jv@hotmail.com>
2022-12-01 16:00:34 -0300

    Send data do Local Storage

--- CRUD Básico/js/script.js
+++ CRUD Básico/js/script.js
@@ -5,7 +5,7 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
 const tempClient = {
-    nome: "João",
+    nome: "Victor",
     email: "franca.jv@hotmail.com",
     phone: "11998132120",
     city: "São Paulo"
@@ -14,7 +14,9 @@ const tempClient = {
 //CRUD - Create Read Update Delete
 
 const createClient = (client) => {
-    localStorage.setItem("db_client", JSON.stringify(client));
+    const db_client = JSON.parse(localStorage.getItem('db_client'));
+    db_client.push(client);
+    localStorage.setItem('db_client', JSON.stringify(db_client));
 }
 
 
